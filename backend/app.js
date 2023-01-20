9c72191ac6fe1f76a090d4a2fddd3f8cc107ed97
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-19 18:11:15 -0600

    Iniial Commit

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
