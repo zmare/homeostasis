9a690325863956e397a2dfe90836b9d7b36d98d0
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-23 09:56:26 -0600

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,46 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
+
+
+router.use(restoreUser);
+
+// GET /api/restore-user
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
 });
 
+
+// GET /api/require-auth
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
 
 
