8f61c7e924e7f117bd6482b70d2b4dbe165a613a
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-23 15:36:48 -0600

    Updated addColumnsToUsers migration to include options object

--- backend/db/migrations/20230123201413-addColumnsToUsers.js
+++ backend/db/migrations/20230123201413-addColumnsToUsers.js
@@ -1,36 +1,27 @@
 'use strict';
-
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-
     await queryInterface.addColumn('Users', 'firstName',
       {
         type: Sequelize.STRING(50)
-      })
+      }, options)
 
     await queryInterface.addColumn('Users', 'lastName',
       {
         type: Sequelize.STRING(50)
-      })
+      }, options)
 
 
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    options.tableName = "Users";
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
   }
 };
