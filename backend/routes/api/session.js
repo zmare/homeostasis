3eb8505f12fd34de226febe4320aa39c363ca81f
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-23 12:19:37 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,6 +28,16 @@ router.post(
 );
 
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+
 
 
 module.exports = router;
