2bd6aeee259a47439a5d522e6b91d18ae7e36423
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-24 17:43:00 -0600

    updates to spots routers

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { Spot, SpotImages } = require('../../db/models');
+const { Spot, SpotImage, Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     let spots = await Spot.findAll({
-        include: {
-
-        }
+        include: [
+            {
+                model: SpotImage
+            }
+        ]
     })
-
-    let spotImage = await spots[0].getSpotImages();
-    let previewURL = spotImage[0].url;
-
-    spots[0].previewURL = 'image URL';
 
     res.json({ spots });
 })
