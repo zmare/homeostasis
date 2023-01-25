447859967db12041862f1043ec70a8ebce9bf1ba
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-25 14:52:07 -0600

    new changes to route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,10 +9,59 @@ const router = express.Router();
 router.get('/current', requireAuth, async (req, res) => {
     let userId = req.user.id;
 
-    let spots = await Spot.findAll({
+    let spotsList = await Spot.findAll({
         where: {
-            ownerId: userId
+            ownerId: 7
+        },
+        include: [
+            {
+                model: SpotImage
+            },
+            {
+                model: Review,
+            }
+        ]
+    })
+
+    let spots = [];
+    spotsList.forEach(spot => {
+        spots.push(spot.toJSON());
+    })
+
+    spots.forEach(spot => {
+        spot.SpotImages.forEach(image => {
+            if (image.preview === true) {
+                spot.previewImage = image.url;
+            }
+
+            if (image.preview === false) {
+                console.log(spot.id)
+                spot.previewImage = "no image found"
+            }
+
+        })
+
+        delete spot.SpotImages;
+
+        let reviews = spot.Reviews
+
+        if (reviews.length) {
+            let sum = 0;
+            for (let review of reviews) {
+                sum += review.stars;
+            }
+            let avg = sum / reviews.length;
+            spot.avgRating = avg;
+            sum = 0;
         }
+
+        if (!reviews.length) {
+            spot.avgRating = 'no reviews yet'
+        }
+
+        delete spot.Reviews;
+
+
     })
 
     res.json(spots);
