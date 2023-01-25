e0577da44d54f776a1b8987d901ee40962035606
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-25 13:09:10 -0600

    added route for get all spots. not yet complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,52 +1,46 @@
 const express = require('express');
 const sequelize = require('sequelize');
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { Spot, SpotImage, Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let spots = await Spot.findAll({
+    let spotsList = await Spot.findAll({
         include: [
             {
                 model: SpotImage
             },
             {
                 model: Review,
-                attributes: {
-                    include: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]]
-                }
             }
         ]
     })
 
-    let spotsList = [];
-    spots.forEach(spot => {
-        spotsList.push(spot.toJSON());
+    let spots = [];
+    spotsList.forEach(spot => {
+        spots.push(spot.toJSON());
     })
 
-    spotsList.forEach(spot => {
-        spot.Reviews.forEach(review => {
-            spot.avgRating = review.avgRating;
-        })
-        delete spot.Reviews;
-
+    spots.forEach(spot => {
         spot.SpotImages.forEach(image => {
             if (image.preview === true) {
                 spot.previewImage = image.url;
             }
-        })
 
-        if (spot.preview === false) {
-            spot.previewImage = "no image found"
-        }
+            if (image.preview === false) {
+                console.log(spot.id)
+                spot.previewImage = "no image found"
+            }
+
+        })
 
         delete spot.SpotImages;
 
     })
 
-    res.json({ spotsList });
+    res.json({ spots });
 })
 
 
