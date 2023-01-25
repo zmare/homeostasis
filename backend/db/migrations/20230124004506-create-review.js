b904ad249bd6130a932b61d7ba7ff459b8d68576
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-24 18:28:52 -0600

    fixing Render deployment issues in affected files

--- backend/db/migrations/20230124004506-create-review.js
+++ backend/db/migrations/20230124004506-create-review.js
@@ -1,4 +1,9 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -45,9 +50,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = "Reviews"
+    await queryInterface.dropTable(options);
   }
 };
