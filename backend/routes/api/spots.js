d52a09162ab86ec5d1fb6aa8ccd7a8fe20f7315d
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-25 17:01:20 -0600

    completed route for get all spots by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sequelize = require('sequelize');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage, Review } = require('../../db/models');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
@@ -63,6 +63,55 @@ router.get('/current', requireAuth, async (req, res) => {
     })
 
     res.json(spots);
+})
+
+router.get('/:id', requireAuth, async (req, res, next) => {
+    let spotPromise = await Spot.findByPk(req.params.id, {
+        include: [
+            {
+                model: SpotImage,
+                attributes: {
+                    exclude: ['spotId', 'createdAt', 'updatedAt']
+                }
+            }
+        ]
+    })
+
+    if (!spotPromise) {
+        res.statusCode = 404;
+        res.json({
+            message: "Spot couldn't be found",
+            statusCode: res.statusCode
+        })
+    }
+
+    let spot = spotPromise.toJSON();
+
+    let spotOwnerPromise = await spotPromise.getUser();
+    let spotOwner = spotOwnerPromise.toJSON();
+    delete spotOwner.username;
+    spot.Owner = spotOwner;
+
+    let spotReviewsPromise = await spotPromise.getReviews();
+    let reviews = []
+    spotReviewsPromise.forEach(review => {
+        reviews.push(review.toJSON());
+    })
+
+
+    if (reviews.length) {
+        spot.numReviews = reviews.length;
+
+        let sum = 0;
+        for (let review of reviews) {
+            sum += review.stars;
+        }
+
+        spot.avgStarRating = sum / reviews.length;
+
+    }
+
+    res.json(spot)
 })
 
 
