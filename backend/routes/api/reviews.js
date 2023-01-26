add7949a99cd8954e03a144bf5ec40a633235bb6
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-26 13:06:30 -0600

    completed route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,10 +1,73 @@
 const express = require('express');
 const sequelize = require('sequelize');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage, Review, User } = require('../../db/models');
+const { Spot, SpotImage, Review, ReviewImage, User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
+
+// GET Routes
+router.get('/current', requireAuth, async (req, res) => {
+    let userId = req.user.id;
+
+    let reviews = await Review.findAll({
+        where: {
+            userId: userId
+        },
+        include: [
+            {
+                model: User,
+                attributes: {
+                    exclude: ['username', 'hashedPassword', 'createdAt', 'updatedAt', 'email']
+
+                }
+            },
+            {
+                model: ReviewImage,
+                attributes: {
+                    exclude: ['reviewId', 'createdAt', 'updatedAt']
+                }
+            }
+        ]
+    })
+
+    let Reviews = [];
+    for (let review of reviews) {
+        Reviews.push(review.toJSON());
+    };
+
+    for (let review of Reviews) {
+        let spot = await Spot.findByPk(review.spotId, {
+            attributes: {
+                exclude: ['createdAt', 'updatedAt', 'description']
+            }
+        });
+        spot = spot.toJSON();
+
+
+        let spotImages = await SpotImage.findAll({
+            where: {
+                spotId: spot.id
+            },
+            attributes: ['url', 'preview']
+        })
+
+        for (let image of spotImages) {
+            image = image.toJSON();
+
+            if (image.preview === true) {
+                spot.previewURL = image.url;
+            } else {
+                spot.previewURL = 'no image found'
+            }
+        }
+
+        review.Spot = spot;
+
+    }
+
+    res.json({ Reviews });
+})
 
 
 
