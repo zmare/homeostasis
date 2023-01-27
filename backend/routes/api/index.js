2f0a03386a4ee0644e0325cafd2f3701a75d4ebe
Zaineb Marediya <zmarediya@yahoo.com>
2023-01-27 16:47:55 -0600

    completed router for deleting a spot-image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const spotImagesRouter = require('./spot-images.js')
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 const { Spot, SpotImage, Review, ReviewImage } = require('../../db/models')
@@ -17,6 +18,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/spot-images', spotImagesRouter)
 
 // GET /api/restore-user
 router.get(
--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -6,5 +6,45 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    let spotImage = await SpotImage.findByPk(req.params.imageId);
+
+    if (!spotImage) {
+        res.statusCode = 404;
+        res.json({
+            message: "Spot image couldn't be found",
+            statusCode: res.statusCode
+        })
+    }
+
+    let spot = await spotImage.getSpot();
+    spot = spot.toJSON();
+    let owner = spot.ownerId;
+
+    //authorization check
+    if (owner !== req.user.id) {
+        res.statusCode = 403;
+        res.json({
+            message: 'Forbidden',
+            statusCode: res.statusCode
+        })
+    } else {
+        spotImage.destroy();
+
+        res.statusCode = 200;
+        res.json({
+            message: "Successfully deleted",
+            statusCode: res.statusCode
+        })
+    }
+
+    res.json('success')
+
+
+
+
+
+})
+
 
 module.exports = router;
