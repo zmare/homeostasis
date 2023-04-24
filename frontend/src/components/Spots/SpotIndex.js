5755b0fe91d6e9869065c0039c21ff0cb942bc4d
Zaineb Marediya <zmarediya@yahoo.com>
2023-04-24 11:49:15 -0500

    mapbox

--- frontend/src/components/Spots/SpotIndex.js
+++ frontend/src/components/Spots/SpotIndex.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getSpots } from '../../store/spots';
+import MapBox from '../MapBox';
 import SpotCard from './SpotCard';
 import './Spots.css'
 
@@ -17,8 +18,9 @@ const SpotIndex = () => {
     spots = Object.values(spots);
 
     return (
-        <>
-            <div className="spot_container_parent">
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                {/* <div className="spot_container_parent"> */}
                 <div className='spot_container'>
                     {spots.map(spot => (
                         <NavLink id={`spot-card-${spot.id}`} className='spot_card_link' key={spot.id} to={`/spots/${spot.id}`}>
@@ -26,11 +28,16 @@ const SpotIndex = () => {
                         </NavLink>
                     ))}
                 </div>
+                {/* </div> */}
+                <div style={{ marginTop: '40px', display: 'inline-block', right: '50px' }}>
+                    <MapBox />
+                </div>
             </div>
+
             <div>
                 <p>footer</p>
             </div>
-        </>
+        </div>
 
 
     );
