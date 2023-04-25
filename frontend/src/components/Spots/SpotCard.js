17cac7a98c363962aea4ee0e978cefbf4efad217
Zaineb Marediya <zmarediya@yahoo.com>
2023-04-25 15:45:16 -0500

    new styling changes

--- frontend/src/components/Spots/SpotCard.js
+++ frontend/src/components/Spots/SpotCard.js
@@ -1,16 +1,18 @@
+import { useState, useEffect } from 'react';
 import "./Spots.css";
 
 const SpotCard = ({ spot }) => {
-    // if (spot.previewImage === 'no image found' || spot.previewImage === 'image testing url') {
-    //     spot.previewImage = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png'
-    // }
+    if (spot.previewImage === 'no image found' || spot.previewImage === 'image testing url') {
+        spot.previewImage = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png'
+    }
 
     if (spot.avgRating === 'no reviews yet') spot.avgRating = 'New';
 
+    const [like, setLike] = useState(false)
+
     const handleLike = (e) => {
         e.preventDefault();
-
-        window.alert("hi");
+        setLike(!like);
 
     }
     return (
@@ -25,8 +27,15 @@ const SpotCard = ({ spot }) => {
             </div>
             <div className='spot_card_price_heart'>
                 <span><span className='spot_card_price'>${spot.price}</span> night</span>
-                <button onClick={handleLike}><i className="fa-regular fa-heart"></i></button>
+                <button className="show-like" onClick={handleLike}>
+                    {
+                        like ?
+                            <i class="fa-solid fa-heart"></i>
+                            :
+                            <i className="fa-regular fa-heart"></i>
 
+                    }
+                </button>
             </div>
         </div >
     );
