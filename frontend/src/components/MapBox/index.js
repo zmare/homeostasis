7d8d89395d6668342f9b11d9c8e1d249a0e15a43
Zaineb Marediya <zmarediya@yahoo.com>
2023-04-25 17:01:55 -0500

    added mapbox

--- frontend/src/components/MapBox/index.js
+++ frontend/src/components/MapBox/index.js
@@ -1,49 +1,29 @@
-import React, { useRef, useEffect, useState } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import mapboxgl from '!mapbox-gl';
-import "./MapBox.css"
+import * as React from 'react';
+import Map, { Marker } from 'react-map-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiem1hcmUiLCJhIjoiY2xndHRvYXRpMjZmbDN0bHUxZ3lzZWZrYiJ9.NM7sD1vPmMv4I3UffYSq6g';
-
-
-const MapBox = (spot) => {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(spot.spot.lng);
-    const [lat, setLat] = useState(spot.spot.lat);
-    const [zoom, setZoom] = useState(9);
-
-
-
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
-
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
-
+const MapBox = ({ spot }) => {
     return (
-        <div>
-            <div className="sidebar-mapbox">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div>
-            <div ref={mapContainer} className="map-container" />
-        </div>
+        <Map
+            initialViewState={{
+                longitude: spot.lng,
+                latitude: spot.lat,
+                zoom: 14
+            }}
+            style={{ width: 1000, height: 500 }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken='pk.eyJ1Ijoiem1hcmUiLCJhIjoiY2xndHRvYXRpMjZmbDN0bHUxZ3lzZWZrYiJ9.NM7sD1vPmMv4I3UffYSq6g'
+        >
+            <Marker
+                latitude={spot.lat}
+                longitude={spot.lng}
+                offsetLeft={-20}
+                offsetTop={-10}>
+            </Marker>
+        </Map>
     );
-
 }
+
+
+
 
 export default MapBox;
